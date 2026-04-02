a3240a81bc55b41111429a3482e692b724157480
Subhashis <subhashisdhara123@gmail.com>
2026-04-02 10:10:42 +0530

    Add Services, Controller, Responses

--- src/main/java/net/subhashis/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/subhashis/journalApp/controller/JournalEntryController.java
@@ -1,42 +1,69 @@
 package net.subhashis.journalApp.controller;
 
 import net.subhashis.journalApp.entity.JournalEntry;
+import net.subhashis.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/_journal")
+@RequestMapping("/journal")
 public class JournalEntryController {
 
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+    @Autowired
+    private JournalEntryService journalEntryService;
 
     @GetMapping
-    public List<JournalEntry> getAll() {  // localhost:8080/journal GET
-        return new ArrayList<>(journalEntries.values());
+    public ResponseEntity<?> getAll() {  // localhost:8080/journal GET
+        List<JournalEntry> all = journalEntryService.getAll();
+        if (all != null && !all.isEmpty()) {
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry myEntry) {   // localhost:8080/journal POST
-        journalEntries.put(myEntry.getId(), myEntry);
-        return true;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) {   // localhost:8080/journal POST
+        try {
+            myEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry( myEntry );
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @GetMapping("id/{myId}")
-    public JournalEntry getJournalEntryById(@PathVariable long myId) {
-        return journalEntries.get(myId);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId myId) {
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(myId);
+        if (journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{myId}")
-    public JournalEntry deleteJournalEntryById(@PathVariable long myId) {
-        return journalEntries.remove(myId);
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId) {
+        journalEntryService.deleteById(myId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("/id/{id}")
-    public JournalEntry updateJournalById(@PathVariable long id, @RequestBody JournalEntry myEntry) {
-        return journalEntries.put(id, myEntry);
+    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry) {
+        JournalEntry old = journalEntryService.findById(id).orElse(null);
+        if (old != null) {
+            old.setTitle((newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle()));
+            old.setContent((newEntry.getContent() != null && !newEntry.getContent().equals("") ? newEntry.getContent() : old.getContent()));
+            journalEntryService.saveEntry( old );
+            return new ResponseEntity<>(old, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/net/subhashis/journalApp/repositry/JournalEntryRepository.java
+++ src/main/java/net/subhashis/journalApp/repositry/JournalEntryRepository.java
@@ -1,5 +1,9 @@
 package net.subhashis.journalApp.repositry;
 
-public interface JournalEntryRepository {
+import net.subhashis.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
 
 }
